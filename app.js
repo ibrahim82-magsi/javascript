1511f06fdb72028b03d8ff6e1a7f74123e895390
= <=>
2025-07-12 10:38:31 +0500

    updated

--- app.js
+++ app.js
@@ -13,15 +13,30 @@
 // console.log(equal);
 
 
-let arr = [2, 2, 2, 2, 2];
-let equal = true;
+// let arr = [2, 2, 2, 2, 2];
+// let equal = true;
 
-for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== arr[0]) {
-        equal = false;
-        break
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] !== arr[0]) {
+//         equal = false;
+//         break
 
-    }
+//     }
+// }
+
+// console.log(equal);
+
+
+// Create a loop that counts how many numbers in [5, 10, 15, 20, 25] are greater than or
+// equal to the average of the array.
+
+let arr = [5, 10, 15, 20, 25];
+
+let sum = arr.reduce((a, b) => a + b, 0);
+
+for (let i= 0 ; i < arr.length; i++){
+    let sum = 0;
 }
 
-console.log(equal);
+console.log(sum);
+
